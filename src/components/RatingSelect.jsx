d1937f267743cb0a3dbd81269fc3099b2f0de81f
shayanfarzi <farzishayan@gmail.com>
2022-04-12 21:21:17 +0430

    ratingSelect component

--- src/components/RatingSelect.jsx
+++ src/components/RatingSelect.jsx
@@ -4,9 +4,21 @@ function RatingSelect({ select, selected }) {
   };
 
   return (
-    <div>
-      <div></div>
-    </div>
+    <ul className='rating'>
+      {Array.from({ length: 10 }, (_, i) => (
+        <li key={`rating-${i + 1}`}>
+          <input
+            type='radio'
+            id={`num${i + 1}`}
+            name='rating'
+            value={i + 1}
+            onChange={handleChange}
+            checked={selected === i + 1}
+          />
+          <label htmlFor={`num${i + 1}`}>{i + 1}</label>
+        </li>
+      ))}
+    </ul>
   );
 }
 
