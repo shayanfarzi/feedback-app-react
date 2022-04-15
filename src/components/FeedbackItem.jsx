0c4d1a0ec5f2376e6ce7bd3568886307626c1713
shayanfarzi <farzishayan@gmail.com>
2022-04-15 19:08:38 +0430

    card component

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,14 +1,16 @@
 // import { useState } from "react";
 
+import Card from "./shared/Card";
+
 function FeedbackItem({ text, rating }) {
   // const [rating, setRating] = useState(7);
   // const [text, setText] = useState("this is feedback");
 
   return (
-    <div className='card'>
+    <Card reverse={true}>
       <div className='num-display'>{rating}</div>
       <div className='text-display'>{text}</div>
-    </div>
+    </Card>
   );
 }
 
