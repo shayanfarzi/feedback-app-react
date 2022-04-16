9901214e2501840e4254d52cc118407600033de3
shayanfarzi <farzishayan@gmail.com>
2022-04-16 21:17:06 +0430

    btn component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Button from "./shared/Button";
 import Card from "./shared/Card";
 
 function FeedbackForm() {
@@ -18,7 +19,7 @@ function FeedbackForm() {
             placeholder='Write a rewiew'
             value={text}
           />
-          <button type='submit'>Send</button>
+          <Button type='submit'>Send</Button>
         </div>
       </form>
     </Card>
