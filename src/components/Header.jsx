8ed352d28281b7f3d3c404ca2f84a93010eeb6be
shayanfarzi <farzishayan@gmail.com>
2022-04-27 17:41:27 +0430

    some change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,29 @@
 import PropTypes from "prop-types";
-
+import { Link } from "react-router-dom";
 function Header({ text }) {
   const headerStyle = {
     backgroundColor: "rgba(0,0,0,0.4)",
     color: "rgb(66, 145, 235)",
+    width: "100%",
   };
   return (
     <header style={headerStyle}>
-      <div className='container'>
-        <h2>{text}</h2>
+      <div
+        className='container'
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          width: "100%",
+        }}
+      >
+        <Link to='/' style={{ color: "#ff6a95", textDecoration: "none" }}>
+          <h2>{text}</h2>
+        </Link>
+        <Link to='/about' style={{ color: "#fff" }}>
+          {" "}
+          About{" "}
+        </Link>
       </div>
     </header>
   );
